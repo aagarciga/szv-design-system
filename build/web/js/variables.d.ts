6ab286db8393c9f3391e2bd097b9509424019824
Alex Alvarez Gárciga <aagarciga@gmail.com>
2022-10-10 19:16:40 -0400

    dictionary web setup

---
+++ build/web/js/variables.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 10 Oct 2022 23:15:20 GMT
+ */
+
+export const GlobalPrimary : string;
+export const GlobalSecondary : string;
+export const GlobalTertiary : string;
---
+++ build/web/js/variables.js
@@ -0,0 +1,8 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 10 Oct 2022 23:15:20 GMT
+ */
+
+export const GlobalPrimary = "#0e7ac0";
+export const GlobalSecondary = "#f08f24";
+export const GlobalTertiary = "#aec8e8";
