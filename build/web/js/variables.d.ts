ede5ccd4eb7776d2211480161e9e21bca37ca925
Alex Alvarez Gárciga <aagarciga@gmail.com>
2022-10-11 09:42:17 -0400

    Test from code to figma

--- build/web/js/variables.d.ts
+++ build/web/js/variables.d.ts
@@ -1,8 +1,18 @@
 /**
  * Do not edit directly
- * Generated on Mon, 10 Oct 2022 23:15:20 GMT
+ * Generated on Tue, 11 Oct 2022 13:30:28 GMT
  */
 
-export const GlobalPrimary : string;
-export const GlobalSecondary : string;
-export const GlobalTertiary : string;
+export const GlobalColorBrandPrimary : string;
+export const GlobalColorBrandSecondary : string;
+export const GlobalColorBrandTertiary : string;
+export const GlobalColorPrimary : string;
+export const GlobalColorSecondary : string;
+export const GlobalColorTertiary : string;
+export const GlobalSeparation1 : string;
+export const GlobalSeparation2 : string;
+export const GlobalSeparation3 : string;
+export const GlobalSeparation4 : string;
+export const GlobalSeparation5 : string;
+export const GlobalSeparation6 : string;
+export const GlobalSeparation7 : string;
